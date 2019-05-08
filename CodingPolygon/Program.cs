b5a413e0504265e1183f893261eec317b9559528
Tigran Balanyan <tbalanyan@gmail.com>
2019-05-07 21:03:26 -0700

    Add project files.

---
+++ CodingPolygon/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingPolygon
+{
+    partial class Program
+    {
+
+        static void Main(string[] args)
+        {
+        }
+        public static Delegate a(Delegate b)
+        {
+            return null
+        }
+    }
+}
